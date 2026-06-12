550eb354358eb45aa83cde2b93052de133ccf5c3
asteria32334 <asteria.32334@gmail.com>
2026-06-12 13:11:57 +0000

    Fix base href - final

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2273613145" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "422463837" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
